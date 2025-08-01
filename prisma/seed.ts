b39de79ace62ac7ae0d08a512035478b42f875f2
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-08-01 16:41:56 -0300

    create seed for times

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/database/prisma";
+
+async function seed() {
+  await prisma.time.createMany({
+    data: [
+      { time: "07:00" },
+      { time: "08:00" },
+      { time: "09:00" },
+      { time: "10:00" },
+      { time: "11:00" },
+      { time: "12:00" },
+      { time: "13:00" },
+      { time: "14:00" },
+      { time: "15:00" },
+      { time: "16:00" },
+      { time: "17:00" },
+      { time: "18:00" },
+      { time: "19:00" },
+      { time: "20:00" },
+      { time: "21:00" },
+      { time: "22:00" },
+      { time: "23:00" },
+    ],
+  });
+}
+
+seed().then(() => {
+  console.log("Db seeded!")
+  prisma.$disconnect()
+});
